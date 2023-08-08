5dec94bcafc24b9c4e2683708fecc5b964e2ef1d
taeminahn <ahntaemin@naver.com>
2023-08-08 17:24:24 +0900

    230808 codeup 2

--- src/inout/Basic98.java
+++ src/inout/Basic98.java
@@ -7,12 +7,33 @@ public class Basic98 {
         int w = scanner.nextInt();
         int h = scanner.nextInt();
         int n = scanner.nextInt();
+        int[][] arr = new int[w][h];
+
+        int[][] barArr = new int[n][4];
 
         for(int i = 0; i < n; i++){
-            int l = scanner.nextInt();
-            int d = scanner.nextInt();
+
+            int length = scanner.nextInt();
+            int distance = scanner.nextInt();
             int x = scanner.nextInt();
             int y = scanner.nextInt();
+
+            for(int j = 0; j < 3; j++){
+                for(int k = 0; k < length; k++) {
+                    if(distance == 1){ // 가로
+                        arr[(x - 1) + k][y - 1] = 1;
+                    }else if(distance == 0) { // 세로
+                        arr[x - 1][(y - 1) + k] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
