6a5fc6370aa3472bc32792724ae9bd471d4a2601
taeminahn <ahntaemin@naver.com>
2023-08-08 20:51:29 +0900

    230808 codeup 4

--- src/inout/Basic99.java
+++ src/inout/Basic99.java
@@ -4,34 +4,28 @@ public class Basic99 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int w = scanner.nextInt();
-        int h = scanner.nextInt();
-        int n = scanner.nextInt();
-        int[][] arr = new int[w][h];
+        int[][] antHome = new int[10][10];
+        int x = 1;
+        int y = 1;
 
-        int[][] barArr = new int[n][4];
-
-        for(int i = 0; i < n; i++){
-
-            int length = scanner.nextInt();
-            int distance = scanner.nextInt();
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-
-            for(int j = 0; j < 3; j++){
-                for(int k = 0; k < length; k++) {
-                    if(distance == 1){ // 가로
-                        arr[(x - 1) + k][y - 1] = 1;
-                    }else if(distance == 0) { // 세로
-                        arr[x - 1][(y - 1) + k] = 1;
-                    }
-                }
+        for(int i = 0; i < antHome.length; i++) {
+            for(int j = 0; j < antHome[i].length; j++) {
+                antHome[i][j] = scanner.nextInt();
             }
         }
+        
+        for(int i = 0; i < antHome.length; i++) {
+            for(int j = 0; j < antHome[i].length; j++) {
+                if(i >= x && j >= 1){
+                    antHome[i][j] = 9;
+                    if(antHome[i][j + 1] == 1) {
+                        i++;
+                    }else if(antHome[i][j + 1] == 2){
+                        return;
+                    }
+                }
 
-        for (int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
-                System.out.print(arr[i][j] + " ");
+                System.out.print(antHome[i][j] + " ");
             }
             System.out.println();
         }
