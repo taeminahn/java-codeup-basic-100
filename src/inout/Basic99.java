b1ea5d1dd2067ef0c98ce73e6d49879d8b7d8403
taeminahn <ahntaemin@naver.com>
2023-08-08 20:19:06 +0900

    230808 codeup 3

---
+++ src/inout/Basic99.java
@@ -0,0 +1,39 @@
+package inout;
+import java.util.Scanner;
+public class Basic99 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int w = scanner.nextInt();
+        int h = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[][] arr = new int[w][h];
+
+        int[][] barArr = new int[n][4];
+
+        for(int i = 0; i < n; i++){
+
+            int length = scanner.nextInt();
+            int distance = scanner.nextInt();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            for(int j = 0; j < 3; j++){
+                for(int k = 0; k < length; k++) {
+                    if(distance == 1){ // 가로
+                        arr[(x - 1) + k][y - 1] = 1;
+                    }else if(distance == 0) { // 세로
+                        arr[x - 1][(y - 1) + k] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
