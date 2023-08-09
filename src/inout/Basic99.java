48c0e8ab719e4b5fa897ed7004f394a60f60d270
taeminahn <ahntaemin@naver.com>
2023-08-09 10:26:35 +0900

    Codeup Basic100 Complete

--- src/inout/Basic99.java
+++ src/inout/Basic99.java
@@ -13,18 +13,30 @@ public class Basic99 {
                 antHome[i][j] = scanner.nextInt();
             }
         }
-        
+        loopComplete:
         for(int i = 0; i < antHome.length; i++) {
+            loopOut:
             for(int j = 0; j < antHome[i].length; j++) {
-                if(i >= x && j >= 1){
+                if(i >= x && j >= y){
+                    if(antHome[i][j] == 2 || (antHome[i+1][j] == 1 && antHome[i][j + 1] == 1)){
+                        antHome[i][j] = 9;
+                        break loopComplete;
+                    }
                     antHome[i][j] = 9;
                     if(antHome[i][j + 1] == 1) {
-                        i++;
+                        x = i;
+                        y = j;
+                        break loopOut;
                     }else if(antHome[i][j + 1] == 2){
-                        return;
+                        antHome[i][j + 1] = 9;
+                        break loopComplete;
                     }
                 }
+            }
+        }
 
+        for (int i = 0; i < antHome.length; i++){
+            for(int j = 0; j < antHome[i].length; j++){
                 System.out.print(antHome[i][j] + " ");
             }
             System.out.println();
